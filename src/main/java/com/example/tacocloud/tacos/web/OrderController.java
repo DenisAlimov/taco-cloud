c7a1780a895748fc76321b25aa52cf19ac559c65
Denis <de.alimov@mail.ru>
2022-09-30 11:14:48 +0300

    Second chapter done

--- src/main/java/com/example/tacocloud/tacos/web/OrderController.java
+++ src/main/java/com/example/tacocloud/tacos/web/OrderController.java
@@ -2,6 +2,8 @@ package com.example.tacocloud.tacos.web;
 
 import com.example.tacocloud.tacos.TacoOrder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,18 +11,25 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.validation.Valid;
+
 @Slf4j
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
 public class OrderController {
     @GetMapping("/current")
-    public String orderForm() {
+    public String orderForm(Model model) {
+        model.addAttribute("tacoOrder", new TacoOrder());
         return "orderForm";
     }
 
     @PostMapping
-    public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+    public String processOrder(@Valid TacoOrder order, Errors errors,
+                               SessionStatus sessionStatus) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
         log.info("Order submitted: {}", order);
         sessionStatus.setComplete();
         return "redirect:/";
